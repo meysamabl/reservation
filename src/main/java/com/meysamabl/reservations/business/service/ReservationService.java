2e0f581df7765229bc313423985555b74dff6717
Meysam <meysamabl@gmail.com>
2017-12-17 23:47:59 -0500

    create restController

--- src/main/java/com/meysamabl/reservations/business/service/ReservationService.java
+++ src/main/java/com/meysamabl/reservations/business/service/ReservationService.java
@@ -10,11 +10,15 @@ import com.meysamabl.reservations.data.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Service
 public class ReservationService {
 
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     private RoomRepository roomRepository;
     private GuestRepository guestRepository;
     private ReservationRepository reservationRepository;
@@ -27,10 +31,11 @@ public class ReservationService {
         this.reservationRepository = reservationRepository;
     }
 
-    public List<RoomReservation> getRoomReservationsForDate(Date date) {
+    public List<RoomReservation> getRoomReservationsForDate(String dateString){
+        Date date = this.createDateFromDateString(dateString);
         Iterable<Room> rooms = this.roomRepository.findAll();
         Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
-        rooms.forEach(room -> {
+        rooms.forEach(room->{
             RoomReservation roomReservation = new RoomReservation();
             roomReservation.setRoomId(room.getId());
             roomReservation.setRoomName(room.getName());
@@ -38,10 +43,10 @@ public class ReservationService {
             roomReservationMap.put(room.getId(), roomReservation);
         });
         Iterable<Reservation> reservations = this.reservationRepository.findByDate(new java.sql.Date(date.getTime()));
-        if(reservations != null) {
+        if(null!=reservations){
             reservations.forEach(reservation -> {
                 Guest guest = this.guestRepository.findOne(reservation.getGuestId());
-                if(guest!=null) {
+                if(null!=guest){
                     RoomReservation roomReservation = roomReservationMap.get(reservation.getRoomId());
                     roomReservation.setDate(date);
                     roomReservation.setFirstName(guest.getFirstName());
@@ -51,13 +56,24 @@ public class ReservationService {
             });
         }
         List<RoomReservation> roomReservations = new ArrayList<>();
-        for (Long roomId:roomReservationMap.keySet()) {
+        for(Long roomId:roomReservationMap.keySet()){
             roomReservations.add(roomReservationMap.get(roomId));
         }
-
         return roomReservations;
-
     }
 
+    private Date createDateFromDateString(String dateString){
+        Date date = null;
+        if(null!=dateString) {
+            try {
+                date = DATE_FORMAT.parse(dateString);
+            }catch(ParseException pe){
+                date = new Date();
+            }
+        }else{
+            date = new Date();
+        }
+        return date;
+    }
 
 }
--- src/main/java/com/meysamabl/reservations/web/application/ReservationController.java
+++ src/main/java/com/meysamabl/reservations/web/application/ReservationController.java
@@ -19,24 +19,13 @@ import java.util.List;
 @RequestMapping(value = "/reservations")
 public class ReservationController {
 
-    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-
     @Autowired
     private ReservationService reservationService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String getResevations(@RequestParam(value = "date", required = false) String dateString, Model model) {
-        Date date = null;
-        if(dateString!= null) {
-            try {
-                date = DATE_FORMAT.parse(dateString);
-            } catch (ParseException pe) {
-                date = new Date();
-            }
-        } else {
-            date = new Date();
-        }
-        List<RoomReservation> roomReservationList = this.reservationService.getRoomReservationsForDate(date);
+
+        List<RoomReservation> roomReservationList = this.reservationService.getRoomReservationsForDate(dateString);
         model.addAttribute("roomReservations", roomReservationList);
         return "reservations";
     }
